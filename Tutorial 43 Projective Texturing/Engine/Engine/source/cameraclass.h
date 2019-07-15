bbde21ef1a32553b0908e745907c3db16bcc0b7a
sportsradical <peereira@live.com.pt>
2019-07-15 16:55:08 +0100

    Add files via upload

---
+++ Tutorial 43 Projective Texturing/Engine/Engine/source/cameraclass.h
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: cameraclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _CAMERACLASS_H_
+#define _CAMERACLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <DirectXMath.h>
+using namespace DirectX;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: CameraClass
+////////////////////////////////////////////////////////////////////////////////
+class CameraClass
+{
+public:
+	CameraClass();
+	CameraClass(const CameraClass&);
+	~CameraClass();
+
+	void SetPosition(float, float, float);
+	void SetRotation(float, float, float);
+
+	XMFLOAT3 GetPosition();
+	XMFLOAT3 GetRotation();
+
+	void Render();
+	void GetViewMatrix(XMMATRIX&);
+
+private:
+	XMFLOAT3 m_position;
+	XMFLOAT3 m_rotation;
+	XMMATRIX m_viewMatrix;
+};
+
+#endif
---
+++ Tutorial 43 Projective Texturing/Engine/Engine/source/projectionshaderclass.h
@@ -0,0 +1,77 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: projectionshaderclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _PROJECTIONSHADERCLASS_H_
+#define _PROJECTIONSHADERCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <fstream>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+using namespace DirectX;
+using namespace std;
+
+
+#pragma comment(lib, "d3dcompiler.lib")
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: ProjectionShaderClass
+////////////////////////////////////////////////////////////////////////////////
+class ProjectionShaderClass
+{
+private:
+	struct MatrixBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+		XMMATRIX view2;
+		XMMATRIX projection2;
+	};
+
+	struct LightBufferType
+	{
+		XMFLOAT4 ambientColor;
+		XMFLOAT4 diffuseColor;
+		XMFLOAT3 lightDirection;
+		float padding;
+	};
+
+public:
+	ProjectionShaderClass();
+	ProjectionShaderClass(const ProjectionShaderClass&);
+	~ProjectionShaderClass();
+
+	bool Initialize(ID3D11Device* device, HWND hwnd);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext* deviceContext, int indexCount, XMMATRIX worldMatrix, XMMATRIX viewMatrix,
+		XMMATRIX projectionMatrix, ID3D11ShaderResourceView* texture, XMFLOAT4 ambientColor, XMFLOAT4 diffuseColor,
+		XMFLOAT3 lightDirection, XMMATRIX viewMatrix2, XMMATRIX projectionMatrix2,
+		ID3D11ShaderResourceView* projectionTexture);
+
+private:
+	bool InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vsFilename, WCHAR* psFilename);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob* errorMessage, HWND hwnd, WCHAR* shaderFilename);
+
+	bool SetShaderParameters(ID3D11DeviceContext* deviceContext, XMMATRIX worldMatrix, XMMATRIX viewMatrix,
+		XMMATRIX projectionMatrix, ID3D11ShaderResourceView* texture, XMFLOAT4 ambientColor,
+		XMFLOAT4 diffuseColor, XMFLOAT3 lightDirection, XMMATRIX viewMatrix2, XMMATRIX projectionMatrix2,
+		ID3D11ShaderResourceView* projectionTexture);
+
+	void RenderShader(ID3D11DeviceContext* deviceContext, int indexCount);
+
+private:
+	ID3D11VertexShader* m_vertexShader;
+	ID3D11PixelShader* m_pixelShader;
+	ID3D11InputLayout* m_layout;
+	ID3D11SamplerState* m_sampleState;
+	ID3D11Buffer* m_matrixBuffer;
+	ID3D11Buffer* m_lightBuffer;
+};
+
+#endif
